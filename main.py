0b251528a259ee35c8059d42863a0ed264da6085
Kseniya <ksenybialkoxs@gmail.com>
2024-09-23 21:18:14 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+n = int(input("Введите количество школьников "))
+k = int(input("Введите количество яблок "))
+applesperstudent =k // n
+appelsinkorzinka = k % n
+print("Яблок досталось каждому школьнику", applesperstudent,  )
+print("яблок осталось в корзинке", appelsinkorzinka, )
